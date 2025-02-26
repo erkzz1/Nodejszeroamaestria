9d1221e2b8cd0269ef01fc3a8428664d6bcf4ccb
erkzz1 <erickmathias.contato@gmail.com>
2025-02-26 16:45:11 -0300

    feat: adicionar validação de entrada e status nas respostas da API para criação de produtos

--- 14_API/3_status_na_resposta/index.js
+++ 14_API/3_status_na_resposta/index.js
@@ -13,12 +13,19 @@ app.post('/createproduct', (req, res) => {
   const name = req.body.name
   const price = req.body.price
 
+  if (!name) {
+    res.status(422).json({ message: 'O campo nome é obrigatório!' })
+    return
+  }
+
   console.log(name)
   console.log(price)
 
-  res.status(201).json({
-    message: `o produto ${name} foi criado com sucesso! Custando ${price}`,
-  })
+  res
+    .status(201)
+    .json({
+      message: `o produto ${name} foi criado com sucesso! Custando ${price}`,
+    })
 })
 
 //rotas - endpoints
