178976697d313cf2b79f7543279ad8e5635f7449
erkzz1 <erickmathias.contato@gmail.com>
2025-03-14 16:30:35 -0300

    :sparkles: feat: adicionar funcionalidade para verificar usuário autenticado e criar helper para obter token

--- 15_GET_A_PET/backend/controllers/UserController.js
+++ 15_GET_A_PET/backend/controllers/UserController.js
@@ -2,7 +2,12 @@ const User = require('../models/User')
 
 const bcrypt = require('bcrypt')
 
+const jwt = require('jsonwebtoken')
+
+
+//helpers
 const createUserToken = require('../helpers/create-user-token')
+const getToken = require('../helpers/get-token')
 
 function validateFields(fields, body) {
   for (const [field, message] of Object.entries(fields)) {
@@ -98,5 +103,22 @@ module.exports = class UserController {
     } catch (error) {
       res.status(500).json({ message: error })
     }
+  }
+
+  static async checkUser(req, res) {
+    let currentUser
+
+    if (req.headers.authorization) {
+      const token = getToken(req)
+      const decoded = jwt.verify(token, 'nossosecret')
+
+      currentUser = await User.findById(decoded.id)
+
+      currentUser.password = undefined
+    } else {
+      currentUser = null
+    }
+
+    res.status(200).send(currentUser)
   }
 }
