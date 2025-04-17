7bfe53b1b324f0d7efad85754e84c9b4881e0183
erkzz1 <erickmathias.contato@gmail.com>
2025-04-17 17:37:18 -0300

    :sparkles: feat: adicionar estrutura básica do front-end com rotas e componentes principais

--- 15_GET_A_PET/frontend/src/App.js
+++ 15_GET_A_PET/frontend/src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+/* Components */
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+
+/* Pages */
+import Login from './components/pages/Auth/Login'
+import Register from './components/pages/Auth/Register'
+import Home from './components/pages/Home'
+
 function App() {
-  return <div className="App">
-    <h1>Get A Pet</h1>
-    
-  </div>
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
---
+++ 15_GET_A_PET/frontend/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+   <footer>
+    <p><span className="bold">Get A Pet</span> &copy; 2025</p>
+   </footer>
+  )
+}
+
+export default Footer
